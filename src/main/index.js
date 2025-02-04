811ed42ede25056f7516ee745b6ac3dfdac1b334
Nazeb Zurati <nazeb04@gmail.com>
2025-02-04 16:23:58 +0800

    Improve icons
    + Remove AppImage build
    + Add rpm package build

--- src/main/index.js
+++ src/main/index.js
@@ -14,7 +14,7 @@ function createWindow() {
     show: false,
     resizable: false,
     autoHideMenuBar: true,
-    ...(process.platform === 'linux' ? { icon } : {}),
+    icon: icon,
     webPreferences: {
       preload: join(__dirname, '../preload/index.js'),
       sandbox: false
