a337a61a7df95285eb80cf662554893371b19ad4
Nazeb Zurati <nazeb04@gmail.com>
2025-03-09 22:50:47 +0800

    Add tauri-plugin-theme

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    let mut ctx = tauri::generate_context!();
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .run(tauri::generate_context!())
+        .plugin(tauri_plugin_theme::init(ctx.config_mut()))
+        .run(ctx)
         .expect("error while running tauri application");
 }
