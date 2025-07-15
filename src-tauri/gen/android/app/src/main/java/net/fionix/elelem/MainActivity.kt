1dc1845f942fa23423c8fe5877874ef72499646c
Nazeb Zurati <nazeb04@gmail.com>
2025-07-15 10:09:41 +0800

    Fix app overlap with statusbar

--- src-tauri/gen/android/app/src/main/java/net/fionix/elelem/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/net/fionix/elelem/MainActivity.kt
@@ -1,3 +1,21 @@
 package net.fionix.elelem
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : TauriActivity() {
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        // Enable edge-to-edge layout
+        window.decorView.let { decorView ->
+            ViewCompat.setOnApplyWindowInsetsListener(decorView) { view, insets ->
+                val sysInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                view.setPadding(sysInsets.left, sysInsets.top, sysInsets.right, sysInsets.bottom)
+                insets
+            }
+        }
+    }
+}
